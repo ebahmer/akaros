e32fc961c9521cba534757cad7ca5204c4ea0be4
Ronald G. Minnich <rminnich@gmail.com>
2015-09-01 10:42:48 -0700

    Clean up apic handling
    We just make the registers an array, not a struct. We don't care about
    most of the values anyway.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- user/vmm/apic.c
+++ user/vmm/apic.c
@@ -39,145 +39,109 @@ struct apicinfo {
 	int id;
 	uint64_t apicbase;
 	uint64_t ioapicbase;
-
-	uint32_t dfr;
-	uint32_t icrhigh;
-	uint32_t icrlow;
-	uint32_t irr[8];
-	uint32_t lvterr;
-	uint32_t lvttimer;
-	uint32_t icount;
 };
 
 static struct apicinfo apicinfo;
 
-char *apic_names[256] = {
-[0x00] "Reserved",
-[0x01] "Reserved",
-[0x02] "Local APIC ID Register Read/Write.",
-[0x03] "Local APIC Version Register Read Only.",
-[0x04] "Reserved",
-[0x05] "Reserved",
-[0x06] "Reserved",
-[0x07] "Reserved",
-[0x08] "Task Priority Register (TPR) Read/Write.",
-[0x09] "Arbitration Priority Register1 (APR) Read Only.",
-[0x0A] "Processor Priority Register (PPR) Read Only.",
-[0x0B] "EOI Register Write Only.",
-[0x0C] "Remote Read Register1 (RRD) Read Only",
-[0x0D] "Logical Destination Register Read/Write.",
-[0x0E] "Destination Format Register Read/Write (see Section",
-[0x0F] "Spurious Interrupt Vector Register Read/Write (see Section 10.9.",
-[0x10] "In-Service Register (ISR); bits 31:0 Read Only.",
-[0x11] "In-Service Register (ISR); bits 63:32 Read Only.",
-[0x12] "In-Service Register (ISR); bits 95:64 Read Only.",
-[0x13] "In-Service Register (ISR); bits 127:96 Read Only.",
-[0x14] "In-Service Register (ISR); bits 159:128 Read Only.",
-[0x15] "In-Service Register (ISR); bits 191:160 Read Only.",
-[0x16] "In-Service Register (ISR); bits 223:192 Read Only.",
-[0x17] "In-Service Register (ISR); bits 255:224 Read Only.",
-[0x18] "Trigger Mode Register (TMR); bits 31:0 Read Only.",
-[0x19] "Trigger Mode Register (TMR); bits 63:32 Read Only.",
-[0x1A] "Trigger Mode Register (TMR); bits 95:64 Read Only.",
-[0x1B] "Trigger Mode Register (TMR); bits 127:96 Read Only.",
-[0x1C] "Trigger Mode Register (TMR); bits 159:128 Read Only.",
-[0x1D] "Trigger Mode Register (TMR); bits 191:160 Read Only.",
-[0x1E] "Trigger Mode Register (TMR); bits 223:192 Read Only.",
-[0x1F] "Trigger Mode Register (TMR); bits 255:224 Read Only.",
-[0x20] "Interrupt Request Register (IRR); bits 31:0 Read Only.",
-[0x21] "Interrupt Request Register (IRR); bits 63:32 Read Only.",
-[0x22] "Interrupt Request Register (IRR); bits 95:64 Read Only.",
-[0x23] "Interrupt Request Register (IRR); bits 127:96 Read Only.",
-[0x24] "Interrupt Request Register (IRR); bits 159:128 Read Only.",
-[0x25] "Interrupt Request Register (IRR); bits 191:160 Read Only.",
-[0x26] "Interrupt Request Register (IRR); bits 223:192 Read Only.",
-[0x27] "Interrupt Request Register (IRR); bits 255:224 Read Only.",
-[0x28] "Error Status Register Read Only.",
-[0x29 ] "Reserved",
-[0x2a] "Reserved",
-[0x2b] "Reserved",
-[0x2c] "Reserved",
-[0x2d] "Reserved",
-[0x2E] "Reserved",
-[0x2F] "LVT CMCI Register Read/Write.",
-[0x30] "Interrupt Command Register (ICR); bits 0-31 Read/Write.",
-[0x31] "Interrupt Command Register (ICR); bits 32-63 Read/Write.",
-[0x32] "LVT Timer Register Read/Write.",
-[0x33] "LVT Thermal Sensor Register2 Read/Write.",
-[0x34] "LVT Performance Monitoring Counters Register3 Read/Write.",
-[0x35] "LVT LINT0 Register Read/Write.",
-[0x36] "LVT LINT1 Register Read/Write.",
-[0x37] "LVT Error Register Read/Write.",
-[0x38] "Initial Count Register (for Timer) Read/Write.",
-[0x39] "Current Count Register (for Timer) Read Only.",
-[0x3A] "Reserved",
-[0x3a]"Reserved",
-[0x3b]"Reserved",
-[0x3c]"Reserved",
-[0x3D]"Reserved",
-[0x3E] "Divide Configuration Register (for Timer) Read/Write.",
-[0x3F] "Reserved",
+enum {
+	reserved,
+	readonly = 1,
+	readwrite = 3,
+	writeonly = 2
+};
+
+struct {
+	char *name;
+	int mode;
+	uint32_t value;
+} apicregs[256] = {
+[0x00] {.name = "Reserved", .mode =  reserved},
+[0x01] {.name = "Reserved", .mode =  reserved},
+[0x02] {.name = "Local APIC ID Register Read/Write.", .mode = readwrite},
+[0x03] {.name = "Local APIC Version Register Read Only.", .mode = readonly},
+[0x04] {.name = "Reserved", .mode =  reserved},
+[0x05] {.name = "Reserved", .mode =  reserved},
+[0x06] {.name = "Reserved", .mode =  reserved},
+[0x07] {.name = "Reserved", .mode =  reserved},
+[0x08] {.name = "Task Priority Register (TPR) Read/Write.", .mode = readwrite},
+[0x09] {.name = "Arbitration Priority Register1 (APR) Read Only.", .mode = readonly},
+[0x0A] {.name = "Processor Priority Register (PPR) Read Only.", .mode = readonly},
+[0x0B] {.name = "EOI Register Write Only.", .mode = writeonly},
+[0x0C] {.name = "Remote Read Register1 (RRD) Read Only", .mode = readonly},
+[0x0D] {.name = "Logical Destination Register Read/Write.", .mode = readwrite},
+[0x0E] {.name = "Destination Format Register Read/Write (see Section", .mode = readwrite},
+[0x0F] {.name = "Spurious Interrupt Vector Register Read/Write (see Section 10.9.", .mode = readwrite},
+[0x10] {.name = "In-Service Register (ISR); bits 31:0 Read Only.", .mode = readonly},
+[0x11] {.name = "In-Service Register (ISR); bits 63:32 Read Only.", .mode = readonly},
+[0x12] {.name = "In-Service Register (ISR); bits 95:64 Read Only.", .mode = readonly},
+[0x13] {.name = "In-Service Register (ISR); bits 127:96 Read Only.", .mode = readonly},
+[0x14] {.name = "In-Service Register (ISR); bits 159:128 Read Only.", .mode = readonly},
+[0x15] {.name = "In-Service Register (ISR); bits 191:160 Read Only.", .mode = readonly},
+[0x16] {.name = "In-Service Register (ISR); bits 223:192 Read Only.", .mode = readonly},
+[0x17] {.name = "In-Service Register (ISR); bits 255:224 Read Only.", .mode = readonly},
+[0x18] {.name = "Trigger Mode Register (TMR); bits 31:0 Read Only.", .mode = readonly},
+[0x19] {.name = "Trigger Mode Register (TMR); bits 63:32 Read Only.", .mode = readonly},
+[0x1A] {.name = "Trigger Mode Register (TMR); bits 95:64 Read Only.", .mode = readonly},
+[0x1B] {.name = "Trigger Mode Register (TMR); bits 127:96 Read Only.", .mode = readonly},
+[0x1C] {.name = "Trigger Mode Register (TMR); bits 159:128 Read Only.", .mode = readonly},
+[0x1D] {.name = "Trigger Mode Register (TMR); bits 191:160 Read Only.", .mode = readonly},
+[0x1E] {.name = "Trigger Mode Register (TMR); bits 223:192 Read Only.", .mode = readonly},
+[0x1F] {.name = "Trigger Mode Register (TMR); bits 255:224 Read Only.", .mode = readonly},
+[0x20] {.name = "Interrupt Request Register (IRR); bits 31:0 Read Only.", .mode = readonly},
+[0x21] {.name = "Interrupt Request Register (IRR); bits 63:32 Read Only.", .mode = readonly},
+[0x22] {.name = "Interrupt Request Register (IRR); bits 95:64 Read Only.", .mode = readonly},
+[0x23] {.name = "Interrupt Request Register (IRR); bits 127:96 Read Only.", .mode = readonly},
+[0x24] {.name = "Interrupt Request Register (IRR); bits 159:128 Read Only.", .mode = readonly},
+[0x25] {.name = "Interrupt Request Register (IRR); bits 191:160 Read Only.", .mode = readonly},
+[0x26] {.name = "Interrupt Request Register (IRR); bits 223:192 Read Only.", .mode = readonly},
+[0x27] {.name = "Interrupt Request Register (IRR); bits 255:224 Read Only.", .mode = readonly},
+[0x28] {.name = "Error Status Register Read Only.", .mode = readonly},
+[0x29 ] {.name = "Reserved", .mode =  reserved},
+[0x2a] {.name = "Reserved", .mode =  reserved},
+[0x2b] {.name = "Reserved", .mode =  reserved},
+[0x2c] {.name = "Reserved", .mode =  reserved},
+[0x2d] {.name = "Reserved", .mode =  reserved},
+[0x2E] {.name = "Reserved", .mode =  reserved},
+[0x2F] {.name = "LVT CMCI Register Read/Write.", .mode = readwrite},
+[0x30] {.name = "Interrupt Command Register (ICR); bits 0-31 Read/Write.", .mode = readwrite},
+[0x31] {.name = "Interrupt Command Register (ICR); bits 32-63 Read/Write.", .mode = readwrite},
+[0x32] {.name = "LVT Timer Register Read/Write.", .mode = readwrite},
+[0x33] {.name = "LVT Thermal Sensor Register2 Read/Write.", .mode = readwrite},
+[0x34] {.name = "LVT Performance Monitoring Counters Register3 Read/Write.", .mode = readwrite},
+[0x35] {.name = "LVT LINT0 Register Read/Write.", .mode = readwrite},
+[0x36] {.name = "LVT LINT1 Register Read/Write.", .mode = readwrite},
+[0x37] {.name = "LVT Error Register Read/Write.", .mode = readwrite},
+[0x38] {.name = "Initial Count Register (for Timer) Read/Write.", .mode = readwrite},
+[0x39] {.name = "Current Count Register (for Timer) Read Only.", .mode = readonly},
+[0x3A] {.name = "Reserved", .mode =  reserved},
+[0x3a]{.name = "Reserved", .mode =  reserved},
+[0x3b]{.name = "Reserved", .mode =  reserved},
+[0x3c]{.name = "Reserved", .mode =  reserved},
+[0x3D]{.name = "Reserved", .mode =  reserved},
+[0x3E] {.name = "Divide Configuration Register (for Timer) Read/Write.", .mode = readwrite},
+[0x3F] {.name = "Reserved", .mode =  reserved},
 };
 
 static uint32_t apic_read(uint64_t offset)
 {
 
 	uint32_t low;
-	
-	DPRINTF("apic_read offset %s 0x%x\n", apic_names[offset],(int)offset);
 
-	if (offset >= APIC_CONFIG) {
-		fprintf(stderr, "Whoa. Reading past apic space? What gives?\n");
-		return -1;
+	DPRINTF("apic_read offset %s 0x%x\n", apicregs[offset].name, (int)offset);
+
+	if (! apicregs[offset].mode & 1) {
+		fprintf(stderr, "Attempt to read %s, which is %s\n", apicregs[offset].name,
+			apicregs[offset].mode == 0 ?  "reserved" : "writeonly");
+		// panic? what to do?
+		return (uint32_t) -1;
 	}
-	
-	// We don't use the x16 addresses because the x2apic doesn't have them. This needs to be for x2apics.
+
+	// no special cases yet.
 	switch (offset) {
-	case 0x2: 
-		// return our PIC.
-		return apicinfo.id;
-		break;
-	case 0x3: 
-		return 2; // VERSION?
-		break;
-	case 0xd: 
-		return 0;
-		break;
-	case 0x10: 
-		return 0;
-		break;
-	case 0x20:
-	case 0x21:
-	case 0x22:
-	case 0x23:
-	case 0x24:
-	case 0x25:
-	case 0x26:
-	case 0x27:
-		DPRINTF("%s: return %08x\n", apic_names[offset], apicinfo.irr[offset-0x20]);
-		return apicinfo.irr[offset-0x20];
-		break;
-	case 0x28:
-		return 0;
-		break;
-	case 0x30:
-		return apicinfo.icrlow;
-		break;
-	case 0x31:
-		return apicinfo.icrhigh;
-		break;
-	case 0x32:
-		return apicinfo.lvttimer;
-		break;
-	case 0x37:
-		return apicinfo.lvterr;
-		break;
-	case 0x38:
-		return apicinfo.icount;
-		break;
 	default:
-		fprintf(stderr, "%s: Attempt to read register@0x%x: %s\n", __func__, offset, apic_names[offset]);
-		return 0;
+		DPRINTF("%s: return %08x\n", apicregs[offset].name, apicregs[offset].value);
+		return apicregs[offset].value;
+		break;
 	}
 	return 0;
 }
@@ -186,45 +150,21 @@ static void apic_write(uint64_t offset, uint32_t value)
 {
 	uint64_t val64;
 	uint32_t low, high;
-	
-	DPRINTF("apic_write offset %s 0x%x value 0x%x\n", apic_names[offset], (int)offset, value);
+
+	DPRINTF("apic_write offset %s 0x%x value 0x%x\n", apicregs[offset].name, (int)offset, value);
+
+	if (! apicregs[offset].mode & 2) {
+		fprintf(stderr, "Attempt to write %s, which is %s\n", apicregs[offset].name,
+			apicregs[offset].mode == 0 ?  "reserved" : "readonly");
+		// panic? what to do?
+		return;
+	}
 
 	switch (offset) {
-	case 0x2: 
-		// return our PIC.
-		apicinfo.id = value;
-		break;
-	case 0x20:
-	case 0x21:
-	case 0x22:
-	case 0x23:
-	case 0x24:
-	case 0x25:
-	case 0x26:
-	case 0x27:
-		DPRINTF("%s: set to  %08x\n", apic_names[offset], value);
-		apicinfo.irr[offset-0x20] = value;
-		break;
-	case 0x30:
-		apicinfo.icrlow = value;
-		DPRINTF("ICR LOW: set to %x\n", value>>24);
-		break;
-	case 0x31:
-		apicinfo.icrhigh = value;
-		DPRINTF("ICR HIGH: set dest to %x\n", value>>24);
-		break;
-	case 0x32:
-		apicinfo.lvttimer = value;
-		break;
-	case 0x37:
-		apicinfo.lvterr = value;
-		break;
-	case 0x38:
-		apicinfo.icount = value;
-		break;
 	default:
-		// FIXME ... lots to do here. we only support one apic.
-		fprintf(stderr, "%s: Attempt to write register@0x%x: %s\n", __func__, offset, apic_names[offset]);
+		DPRINTF("%s: Set to %08x\n", apicregs[offset].name, value);
+		apicregs[offset].value = value;
+		break;
 	}
 
 }
@@ -247,10 +187,10 @@ int apic(struct vmctl *v, uint64_t gpa, int destreg, uint64_t *regp, int store)
 
 	if (store) {
 		apic_write(offset, *regp);
-		DPRINTF("Write: mov %s to %s @%p val %p\n", regname(destreg), apic_names[(uint8_t)offset], gpa, *regp);
+		DPRINTF("Write: mov %s to %s @%p val %p\n", regname(destreg), apicregs[offset].name, gpa, *regp);
 	} else {
 		*regp = apic_read(offset);
-		DPRINTF("Read: Set %s from %s @%p to %p\n", regname(destreg), apic_names[(uint8_t)offset], gpa, *regp);
+		DPRINTF("Read: Set %s from %s @%p to %p\n", regname(destreg), apicregs[offset].name, gpa, *regp);
 	}
 
 }
